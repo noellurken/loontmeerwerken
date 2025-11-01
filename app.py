1b81fb529d5772521b8655f1f71f734024583b24
noellurken <noelluerken@me.com>
2025-11-01 14:32:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ import plotly.graph_objects as go
 
 import streamlit as st
 
-st.set_page_config(
-    page_title="My App",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    theme="dark"
-)
+st.set_page_config(theme="dark")
 
 st.title("Loont het om meer te werken? 💰")
 
